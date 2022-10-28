9d9ae34bd7be95c95eefe5a2d0c081c07b4fc58a
alexwallden <alexander.wallden@me.com>
2022-10-28 16:05:38 +0200

    :construction: working on positions

--- js/header.js
+++ js/header.js
@@ -7,10 +7,10 @@ menuButton.addEventListener('click', () => {
     showMenu = !showMenu;
     // navMenu.style = showMenu ? openAttr : closedAttr;
     navMenu.style.visibility = showMenu ? 'visible' : 'hidden';
-    navMenu.style.top = showMenu ? '0' : '-100vh';
-    window.pageYOffset > 87 && (header.style.transition = 'margin-top .3s ease-in-out')
+    navMenu.style.top = showMenu ? '0' : '-155px';
+    window.pageYOffset < 87 && (header.style.transition = 'margin-top .3s ease-in-out')
 
-    header.style.marginTop = showMenu ? '100px' : '0';
+    header.style.marginTop = showMenu ? '155px' : '0';
 
     // window.pageYOffset === 0 && window.scrollTo(0, 100);
 })
